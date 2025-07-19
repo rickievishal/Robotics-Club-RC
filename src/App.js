54239c2528d3a683dac0d0898bb12f1333832380
Vishal Thangamaiappan <vishalthangamaiappan@Vishals-MacBook-Air.local>
2025-07-19 16:30:43 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [ip, setip] = useState("Your esp's ip address")
+  const [ip, setip] = useState("")
   const [ledState, setLedState] = useState(false)
   const turnOn = () => {
       try{
@@ -15,15 +15,35 @@ function App() {
     }
     const turnOff = () => {
       try{
-        const response = axios.get(`http://${ip}/turn/on`)
+        const response = axios.get(`http://${ip}/turn/off`)
       }catch(err){
-        console.log(err)
+        console.log(err);
       }
     }
   return (
-    <div className="App">
-      <input value={ip} onChange={(e) => setip(e.target.value)}/>
-      
+    <div className="App py-16">
+      <h1 className='text-2xl'>
+        Robotics Club
+      </h1>
+      <p className='pt-16'>
+        Ip address of your esp32 network
+      </p>
+      <input className='border px-4' placeholder='Enter the ip address' value={ip} onChange={(e) => setip(e.target.value)}/>
+      <div>
+        <p>
+          LED IS {ledState ?  "on" : "off"}
+        </p>
+        <button className='px-4 py-1 rounded-lg bg-black text-white' onClick={()=> {
+          if(ledState){
+            turnOff()
+          }else{
+            turnOn()
+          }
+        }}>
+            change led state
+        </button>
+
+      </div>
     </div>
   );
 }
